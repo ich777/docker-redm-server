702735e7eef7ef2823d9a4910a26eb2f51eaa788
ich777 <christophhummer@gmail.com>
2021-09-26 08:53:51 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -119,7 +119,7 @@ else
                 sleep infinity
             fi
             tar -xf fx.tar.xz
-            rm ${SERVER_DIR}/$LAT_V/fx.tar.xz
+            rm -R fx.tar.xz
             touch redminstalled-$LAT_V
         elif [ "$LAT_V" == "$CUR_V" ]; then
             echo "---RedM Version up-to-date---"
